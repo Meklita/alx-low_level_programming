5326787b821ba4c24348250718661cc97336aca3
Meklita <mektnz@gmail.com>
2022-07-05 11:42:55 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main- returns _putchar 
+ *
+ * return always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a read me for nested loops project
