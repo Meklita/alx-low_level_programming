fa5ba2aaac4d766de50acbf8fe38a1a8e778e4ea
Meklita <mektnz@gmail.com>
2022-07-07 12:38:54 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,34 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
- *
- * Return: void.
+ * main - program that prints either number
+ * or fizz or buzz or fizzBuzz
+ * Return: returns 0
  */
 int main(void)
 {
-	int i = 1;
+	int num;
 
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
+	while (num++ < 100)
+
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz ");
+
+		else if ((num % 3) == 0)
+			printf("Fizz ");
+
+		else if ((num % 5) == 0)
 		{
-			printf("FizzBuzz");
+			if (num != 100)
+				printf("Buzz ");
+
+			else
+				printf("Buzz");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
+
 		else
-		{
-			printf("%i", i);
-		}
-		if (i != 100)
-		{
-			putchar(' ');
-		}
-		i++;
-	}
-	putchar('\n');
+			printf("%d ", num);
+
+	printf("\n");
 	return (0);
 }
