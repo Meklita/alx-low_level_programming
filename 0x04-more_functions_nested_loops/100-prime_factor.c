607902d299da51fbe56e05da5b046ec8f0bf2b45
Meklita <mektnz@gmail.com>
2022-07-07 12:44:04 -0700

    bonus

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prime numbers
+ *
+ * Return :0
+ */
+int main(void)
+{
+	long int x = 612852475143;
+	long int ab;
+
+	for (ab = 2; ab < x; ab++)
+	{
+		if (x % ab == 0)
+		{
+			x = x / ab;
+		}
+	}
+	printf("%ld\n", ab);
+	return (0);
+}
