078aea22de8733db18b2d7df9d8606a826dde7cc
Meklita <mektnz@gmail.com>
2022-07-18 12:11:29 -0700

    commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,19 +3,22 @@
 /**
  * print_chessboard - print the chessboard, 2d array
  * @a: 2d array of chars
- *Return: void
+ * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	i = 0;
+	while (i < 8)
 	{
-		for (j = 0; j < 8; j++)
+		j = 0;
+		while (j < 8)
 		{
 			_putchar (a[i][j]);
+			j++;
 		}
-		_putchar('\n');
+		_putchar ('\n');
+		i++;
 	}
 }
