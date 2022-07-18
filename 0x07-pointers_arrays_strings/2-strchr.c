7efe8974b748cb554adc0dd79c1ed047095a0056
Meklita <mektnz@gmail.com>
2022-07-18 12:25:27 -0700

    updat

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,39 +1,25 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
+ * _strchr - Locates a character in a string
+ * @s: String
+ * @c: Character to search
+ * Return: Pointer to the first occurrence of the character c in the string s
  */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
-
-	str1 = s;
-	str2 = accept;
-	i = 0;
-
-	while (str1[i] != '\0')
+	while (*s != '\0')
 	{
-		j = 0;
-		while (str2[j] != '\0')
+		if (*s == c)
 		{
-			if (str2[j] == str1[i])
-			{
-				count++;
-				break;
-			}
-			j++;
+			return (s);
 		}
-		if (s[i] != accept[j])
-		{
-			break;
-		}
-		i++;
+		++s;
 	}
-	return (count);
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
 }
