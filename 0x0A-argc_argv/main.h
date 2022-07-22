b2335ff7bee3177d1c3acc54c319697632d2083c
Meklita <mektnz@gmail.com>
2022-07-22 13:36:25 -0700

    main

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char);
 int main(int argc, char **argv);
 int main(int argc, char *argv[]);
 
+#endif
